6d0aadeb66e156ace7e5cf822e2876140303cbee
R. X. Duran <rogolio.duran@gmail.com>
2014-08-25 01:56:03 -0400

    Better Angular app structure

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener("load", function() {
-  var kosblock = angular.module("kosblock", []);
+  
 
   // Decorate the HTML tag with ng-app (Angular root directive) 
   // and ng-csp (Content Security Policy directive)
@@ -11,23 +11,9 @@ window.addEventListener("load", function() {
   // Fix the controller to the comments div
   $('#comments').attr('ng-controller', 'CommentsController');
 
-  // Define the controller
-  kosblock.controller('CommentsController', function ($scope) {
-
-  });
-
+  
   // Append the kosblocker directive underneath all comment footers (div.cf)
   $('p.cl').append('[ <a kosblocker></a> ]');
-
-  // Attach the directive to the kosblocker element
-  kosblock.directive('kosblocker', function() {
-    return {
-      restrict: 'A'
-      , replace: true
-      , template: '<a>Bozo</a>'
-    };
-  });
-
 
   // Bootstrap the page to attach angular and our customizations
   var html = document.getElementsByName('html');
